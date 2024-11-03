9711dfc3a1c99424f9fcd132537a66337638f97a
kol-oss <kolyakhozhajinov@gmail.com>
2024-11-03 20:01:42 +0200

    Add user service and controller

---
+++ src/controller/user_controller.py
@@ -0,0 +1,31 @@
+from flask import request
+
+from src import app
+from src.service import UserService
+
+from src.datasource import user_repository
+
+USER_ROUTE = '/user'
+user_service = UserService(user_repository)
+
+@app.route(USER_ROUTE + 's', methods = ['GET'])
+def get_users():
+    users = user_service.get_all_users()
+
+    return {"users": [user.__dict__ for user in users]}
+
+@app.route(USER_ROUTE + '/<user_id>', methods = ['GET'])
+def get_user(user_id):
+    return user_service.get_user(user_id).__dict__
+
+@app.route(USER_ROUTE, methods = ['POST'])
+def create_user():
+    user_data = request.get_json()
+    name = user_data['name']
+    if name is None or name == "": return {"message": "Please provide a name"}
+
+    return user_service.create_user(name).__dict__
+
+@app.route(USER_ROUTE + '/<user_id>', methods = ['DELETE'])
+def delete_user(user_id):
+    return user_service.delete_user(user_id).__dict__
---
+++ src/service/user_service.py
@@ -0,0 +1,24 @@
+from src.dto.user import User
+from src.repository.user_repository import UserRepository
+
+class UserService:
+    __user_repository = None
+
+    def __init__(self, user_repository: UserRepository):
+        self.__user_repository = user_repository
+
+    def get_user(self, user_id: str) -> User | None:
+        if user_id is None: return None
+
+        return self.__user_repository.get(user_id)
+
+    def get_all_users(self) -> list[User]:
+        return list(self.__user_repository.get_all())
+
+    def create_user(self, name: str) -> User | None:
+        if name is None: return None
+
+        return self.__user_repository.get_or_create(name)
+
+    def delete_user(self, user_id: str) -> None:
+        return self.__user_repository.delete(user_id)
