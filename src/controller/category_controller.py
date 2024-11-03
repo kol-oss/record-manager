6d597d937d51a347a6423022fe5663f218a19afc
kol-oss <kolyakhozhajinov@gmail.com>
2024-11-03 20:01:50 +0200

    Add category service and controller

---
+++ src/controller/category_controller.py
@@ -0,0 +1,25 @@
+from flask import request
+
+from src import app
+from src.service.category_service import CategoryService
+
+from src.datasource import category_repository
+
+CATEGORY_ROUTE = '/category'
+category_service = CategoryService(category_repository)
+
+@app.route(CATEGORY_ROUTE + '/<category_id>', methods = ['GET'])
+def get_category(category_id):
+    return category_service.get_category(category_id).__dict__
+
+@app.route(CATEGORY_ROUTE, methods = ['POST'])
+def create_category():
+    category_data = request.get_json()
+    name = category_data['name']
+    if name is None or name == "": return {"message": "Please provide a name"}
+
+    return category_service.create_category(name).__dict__
+
+@app.route(CATEGORY_ROUTE + '/<category_id>', methods = ['DELETE'])
+def delete_category(category_id):
+    return category_service.delete_category(category_id).__dict__
---
+++ src/service/category_service.py
@@ -0,0 +1,21 @@
+from src.dto.category import Category
+from src.repository.category_repository import CategoryRepository
+
+class CategoryService:
+    __category_repository = None
+
+    def __init__(self, category_repository: CategoryRepository):
+        self.__category_repository = category_repository
+
+    def get_category(self, category_id: str) -> Category | None:
+        if category_id is None: return None
+
+        return self.__category_repository.get(category_id)
+
+    def create_category(self, name: str) -> Category | None:
+        if name is None: return None
+
+        return self.__category_repository.get_or_create(name)
+
+    def delete_category(self, category_id: str) -> None:
+        return self.__category_repository.delete(category_id)
