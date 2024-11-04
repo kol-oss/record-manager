48ceb51b1acd4d16b73ead51ae4990e0bbf99952
kol-oss <kolyakhozhajinov@gmail.com>
2024-11-04 22:35:46 +0200

    Add creation date to record

--- src/dto/record.py
+++ src/dto/record.py
@@ -1,11 +1,15 @@
+import datetime
+
 class Record:
     record_id = None
     user_id = None
     course_id = None
     value = 0
+    creation_date = None
 
     def __init__(self, record_id: str, user_id: str, category_id: str, value: int):
         self.record_id = record_id
         self.user_id = user_id
         self.category_id = category_id
         self.value = value
+        self.creation_date = datetime.datetime.now()
